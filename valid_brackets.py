1d835f253467b0f207c961b487b353aa87c66dae
ashishbajpaicern <105496246+ashishbajpaicern@users.noreply.github.com>
2022-07-18 13:38:48 +0530

    Update valid_brackets.py

--- valid_brackets.py
+++ valid_brackets.py
@@ -1,24 +1,26 @@
 class Solution:
     def isValid(self, s: str) -> bool:
 
-        stack = []
-        for char in s:
-            if char in "({[":
-                stack.append(char)
-            else:
-                if not stack:
-                    return False
-                curr = stack.pop()
-                if curr == "(":
-                    if char != ")":
-                        return False
-                if curr == "{":
-                    if char != "}":
-                        return False
-                if curr == "[":
-                    if char != "]":
-                        return False
+        from collections import deque
 
-        if stack:
-            return False
-        return True
+        l = deque()
+        a = ["{", "[", "("]
+        for i in range(len(s)):
+            if s[i] in a:
+                l.append(s[i])
+            elif s[i] == ")":
+                if not l or "(" not in l:
+                    return False
+                if l[-1] == "(":
+                    l.pop()
+            elif s[i] == "}":
+                if not l or "{" not in l:
+                    return False
+                if l[-1] == "{":
+                    l.pop()
+            elif s[i] == "]":
+                if not l or "[" not in l:
+                    return False
+                if l[-1] == "[":
+                    l.pop()
+        return not l

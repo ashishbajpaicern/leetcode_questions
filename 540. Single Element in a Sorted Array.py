6dc343aa2a083404ad3d923cefd3341fd75b3f60
ashishbajpaicern <105496246+ashishbajpaicern@users.noreply.github.com>
2022-08-02 12:42:11 +0530

    Create 540. Single Element in a Sorted Array.py

---
+++ 540. Single Element in a Sorted Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        stack = []
+        for i in nums:
+            if i not in stack:
+                stack.append(i)
+            else:
+                stack.remove(i)
+        return (stack[0])

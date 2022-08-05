b0a3adc015374e10b1e1e6d93a07651670263ae4
ashishbajpaicern <105496246+ashishbajpaicern@users.noreply.github.com>
2022-08-05 11:08:57 +0530

    Create 75. Sort Colors.py

---
+++ 75. Sort Colors.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero = nums.count(0)
+        one = nums.count(1)
+        two = nums.count(2)
+        for i in range(len(nums)):
+            if zero != 0:
+                nums[i] = 0
+                zero -= 1
+                continue
+            elif one != 0:
+                nums[i] = 1
+                one -= 1
+                continue
+            elif two != 0:
+                nums[i] = 2
+                two -= 1
+                continue

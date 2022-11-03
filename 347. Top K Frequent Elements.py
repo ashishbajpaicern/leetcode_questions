206720b1c8e1155f0beb8e2d9fb245f8540fb7ad
ashishbajpaicern <105496246+ashishbajpaicern@users.noreply.github.com>
2022-11-03 19:41:25 +0530

    Update 347. Top K Frequent Elements.py

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -2,14 +2,8 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         d = {}
         ans = []
-        m = 0
-        check = 0
         for i in nums:
-            if i not in d:
-                d[i] = 1
-            else:
-                d[i] = d[i] + 1
-        print(d)
+            d[i] = 1 + d.get(i, 0)
         for i in range(k):
             max_key = max(d, key=d.get)
             ans.append(max_key)

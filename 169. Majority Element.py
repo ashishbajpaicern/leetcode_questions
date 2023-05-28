1b886ffdb6fb5ccc78be27edd6bb9d0feab10c4f
ashishbajpaicern <105496246+ashishbajpaicern@users.noreply.github.com>
2023-05-28 12:34:26 +0530

    updates

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -1,5 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        for item in set(nums):   # iterating over unique elements with set
-            if nums.count(item) > len(nums) // 2:
-                return item
+        nums.sort()
+        return nums[len(nums) // 2]

04981fcf2ed8a81ad62fbe08200dd48c5e21efe2
ashishbajpaicern <105496246+ashishbajpaicern@users.noreply.github.com>
2023-06-27 00:12:37 +0530

    Update 49. Group Anagrams.py

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -1,3 +1,4 @@
+"""
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d = {}
@@ -11,3 +12,16 @@ class Solution:
         for value in d.values():
             final.append(value)
         return final
+"""
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = defaultdict(list)
+        for i in strs:
+            count = [0] * 26
+            for c in i:
+                count[ord(c) - ord("a")] += 1
+            d[tuple(count)].append(i)
+        print(d)
+        return d.values()
